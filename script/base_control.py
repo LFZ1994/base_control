6b0036b83811883ce6342ca3fb3711279109fefa
LiuFuzhi <lfz1994@qq.com>
2018-12-20 18:48:48 +0800

    add serial Queue

--- script/base_control.py
+++ script/base_control.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=gbk
 import rospy
 import tf
 import time
@@ -10,7 +11,9 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import BatteryState
 
+import Queue
 import ctypes
+
 class BaseControl:
     def __init__(self):
         #Get params
@@ -22,6 +25,16 @@ class BaseControl:
         self.odom_topic = rospy.get_param('~odom_topic','/odom')
         self.battery_topic = rospy.get_param('~battery_topic','battery')
         self.battery_freq = float(rospy.get_param('~battery_freq','0.1'))
+        #define param
+        self.current_time = rospy.Time.now()
+        self.previous_time = self.current_time
+        self.pose_x = 0.0
+        self.pose_y = 0.0
+        self.pose_yaw = 0.0
+        self.serialIDLE_flag = 0
+        self.trans_x = 0.0
+        self.rotat_z = 0.0
+        self.sendcounter = 0
         # Serial Communication
         try:
             self.serial = serial.Serial(self.device_port,self.baudrate,timeout=10)
@@ -44,44 +57,30 @@ class BaseControl:
 
         self.timer_odom = rospy.Timer(rospy.Duration(1.0/self.odom_freq),self.timerOdomCB)
         self.timer_battery = rospy.Timer(rospy.Duration(1.0/self.battery_freq),self.timerBatteryCB)  #20Hz
-        self.timer_cmd = rospy.Timer(rospy.Duration(2.0/self.odom_freq),self.timerCmdCB)
+        # self.timer_cmd = rospy.Timer(rospy.Duration(2.0/self.odom_freq),self.timerCmdCB)
         self.tf_broadcaster = tf.TransformBroadcaster()
+        self.serialQ = Queue.Queue(20)
+        if(self.serial.in_waiting):
+            self.serial.read_all()
+        while True:
+            if self.serial.in_waiting:
+                length = self.serial.in_waiting
+                rospy.logdebug("in_waiting=%d"%length)
+                reading = self.serial.read_all()
+                if len(reading)!=0:
+                    try:
+                        self.serialQ.put(reading)
+                    except:
+                        rospy.logerr("Put Queue Err")
 
-        self.current_time = rospy.Time.now()
-        self.previous_time = self.current_time
-        self.pose_x = 0.0
-        self.pose_y = 0.0
-        self.pose_yaw = 0.0
-        self.serialIDLE_flag = True
-        self.trans_x = 0.0
-        self.rotat_z = 0.0
+
     #Subscribe vel_cmd call this to send vel cmd to move base
     def cmdCB(self,data):
         self.trans_x = data.linear.x
         self.rotat_z = data.angular.z
         # print("cmdCB")
 
-        # while self.serialIDLE_flag==False:
-        #     # time.sleep(1)
-        #     rospy.loginfo("CmdCB Serial Busy")
-        #     time.sleep(0.01)
-        # output = chr(0x5a) + chr(12) + chr(0x01) + chr(0x01) + \
-        #     chr((int(self.trans_x*1000.0)>>8)&0xff) + chr(int(self.trans_x*1000.0)&0xff) + \
-        #     chr(0x00) + chr(0x00) + \
-        #     chr((int(self.rotat_z*1000.0)>>8)&0xff) + chr(int(self.rotat_z*1000.0)&0xff) + \
-        #     chr(0x00) + chr(0x00)
-        # # rospy.logdebug()
 
-        # self.serialIDLE_flag = False
-        # try:
-        #     while self.serial.out_waiting:
-        #         pass
-        #     self.serial.write(output)
-        # except:
-        #     rospy.logerr("Vel Command Send Faild")
-        # self.serialIDLE_flag = True
-        # rospy.loginfo("Send Vel Cmd")
-        # # self.serial.read_all()
 
         
     #Odom Timer call this to get velocity and imu info and convert to odom topic
@@ -89,71 +88,113 @@ class BaseControl:
         #Get move base velocity data
         # rospy.loginfo("Send Encoder Cmd")
         output = chr(0x5a) + chr(0x06) + chr(0x01) + chr(0x03) + chr(0x00) + chr(0x00)
-        while(self.serialIDLE_flag == False):
-            # rospy.loginfo("OdomCB Serial Busy")
+        while(self.serialIDLE_flag):
+            # rospy.loginfo("OdomCB Serial Busy %d"%self.serialIDLE_flag)
             time.sleep(0.01)
-        self.serialIDLE_flag = False
+        self.serialIDLE_flag = 1
         try:
             while self.serial.out_waiting:
                 pass
             self.serial.write(output)
         except:
             rospy.logerr("Odom Command Send Faild")
-        # self.serialIDLE_flag = True   
-        try:
-            # data = self.data
-            while self.serial.in_waiting != 12:
-                pass
-            data = self.serial.read(12)
-            self.serialIDLE_flag = True
-            #Normal mode
-            if(int(data[3].encode('hex'),16) == 0x04):
-                Vx = int(data[4].encode('hex'),16)*256
-                Vx += int(data[5].encode('hex'),16)
-                Vywa = int(data[8].encode('hex'),16)*256
-                Vywa += int(data[9].encode('hex'),16)
-                # rospy.loginfo("Valid Data")
-            else:
-                print len(data),int(data[3].encode('hex'),8)
-                rospy.logerr("Invalid Encoder Data")
-            # self.current_time = rospy.Time.now()
-            # dt = (self.previous_time - self.current_time).to_sec
-            # self.previous_time = self.current_time
-        except :
-            rospy.logerr("Encoder Try Faild")
-            # print data + str(len(data))
-            pass
+        self.serialIDLE_flag = 0   
+
+        while True:
+            try:
+                data = self.serialQ.get(timeout=10)
+                if(len(data)==12):
+                    if(int(data[3].encode('hex'),16) == 0x04):
+                        break
+                    else:
+                        self.serialQ.put(data)
+                        rospy.logerr("Not Encoder Msg")
+                elif (len(data) > 12):
+                    if(int(data[3].encode('hex'),16) == 0x04):
+                        for i in range(0,len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        rospy.logerr("Encoder Pest Other Msg")
+                        for i in range(int(data[1].encode('hex'),16),len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        self.serialQ.put(data[int(data[1].encode('hex'),16):])
+                        break
+                    else:
+                        self.serialQ.put(data)
+                else:
+                    self.serialQ.put(data)
+                    rospy.logerr("Not Encoder Msg Len %d"%len(data))
+            except:
+                rospy.logerr("Encoder Get Q Faild")
+                break
+        #Normal mode
+        if(int(data[3].encode('hex'),16) == 0x04):
+            Vx = int(data[4].encode('hex'),16)*256
+            Vx += int(data[5].encode('hex'),16)
+            Vywa = int(data[8].encode('hex'),16)*256
+            Vywa += int(data[9].encode('hex'),16)
+            # rospy.loginfo("Valid Data")
+        else:
+            print len(data),int(data[3].encode('hex'),8)
+            rospy.logerr("Invalid Encoder Data")
         Vx = float(ctypes.c_int16(Vx).value/1000.0)
         Vywa = float(ctypes.c_int16(Vywa).value/1000.0)
+
         # rospy.loginfo("Send Imu Cmd")
         output = chr(0x5a) + chr(0x06) + chr(0x01) + chr(0x05) + chr(0x00) + chr(0x00)
-        while(self.serialIDLE_flag == False):
-            # rospy.loginfo("OdomCB Serial Busy")
+        while(self.serialIDLE_flag):
+            # rospy.loginfo("OdomCB Serial Busy %d"%self.serialIDLE_flag)
             time.sleep(0.01)
-        self.serialIDLE_flag = False
+        self.serialIDLE_flag = 2
         while self.serial.out_waiting:
             pass
         self.serial.write(output)
  
-        try:
-            while self.serial.in_waiting != 12:
-                pass
-            data = self.serial.read(12)
-            self.serialIDLE_flag = True 
-            #Normal mode
-            if(int(data[3].encode('hex'),16) == 0x06):
-                Yawz = int(data[8].encode('hex'),16)*256
-                Yawz += int(data[9].encode('hex'),16)
-                # print int(Yawz,16)
-                # rospy.loginfo("Valid Data")
-            else:
-                # print len(data),int(data[3].encode('hex'),8)
-                rospy.logerr("Invalid Imu Data")
+        # try:
+        self.serialIDLE_flag = 0 
+        while True:
+            try:
+                data = self.serialQ.get(timeout=10)
+                if(len(data)==12):
+                    if(int(data[3].encode('hex'),16) == 0x06):
+                        break
+                    else:
+                        self.serialQ.put(data)
+                        rospy.logerr("Not Imu Msg")
+                elif (len(data) > 12):
+                    if(int(data[3].encode('hex'),16) == 0x06):
+                        for i in range(0,len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        rospy.logerr("Imu Pest Other Msg")
+                        for i in range(int(data[1].encode('hex'),16),len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        self.serialQ.put(data[int(data[1].encode('hex'),16):])
+                        break
+                    else:
+                        self.serialQ.put(data)
+                else:
+                    self.serialQ.put(data)
+                    rospy.logerr("Not Imu Msg Length %d"%len(data))
+            except:
+                rospy.logerr("Imu Get Q Faild")
+                break
+        #Normal mode
+        if(int(data[3].encode('hex'),16) == 0x06):
+            Yawz = int(data[8].encode('hex'),16)*256
+            Yawz += int(data[9].encode('hex'),16)
+            # print int(Yawz,16)
+            # rospy.loginfo("Valid Data")
+        else:
+            # print len(data),int(data[3].encode('hex'),8)
+            rospy.logerr("Invalid Imu Data")
 
-        except :
-            rospy.logerr("IMU Try Faild")
-            # print data + str(len(data))
-            pass
+        # except :
+        #     rospy.logerr("IMU Try Faild")
+        #     # print data + str(len(data))
+        #     pass
         self.pose_yaw = float(ctypes.c_int16(Yawz).value/100.0)
         self.pose_yaw = self.pose_yaw*math.pi/180.0
         self.current_time = rospy.Time.now()
@@ -188,10 +229,10 @@ class BaseControl:
         #     # time.sleep(1)
         
         output = chr(0x5a) + chr(0x06) + chr(0x01) + chr(0x07) + chr(0x00) + chr(0x00)
-        while(self.serialIDLE_flag == False):
-            # rospy.loginfo("BatteryCB Serial Busy")
+        while(self.serialIDLE_flag):
+            # rospy.loginfo("BatteryCB Serial Busy %d"%self.serialIDLE_flag)
             time.sleep(0.01)
-        self.serialIDLE_flag = False
+        self.serialIDLE_flag = 3
         try:
             while self.serial.out_waiting:
                 pass
@@ -199,29 +240,56 @@ class BaseControl:
         except:
             rospy.logerr("Battery Command Send Faild")
         rospy.loginfo("Get Battery Info")
-        try:
-            # data = self.data
-            while self.serial.in_waiting != 10:
-                pass
-            data = self.serial.read(10)
-            self.serialIDLE_flag = True
-            #Normal mode
-            if(int(data[3].encode('hex'),16) == 0x08):
-                Vvoltage = int(data[4].encode('hex'),16)*256
-                Vvoltage += int(data[5].encode('hex'),16)
-                Icurrent = int(data[6].encode('hex'),16)*256
-                Icurrent += int(data[7].encode('hex'),16)
-                # rospy.loginfo("Valid Data")
-            else:
-                print len(data),int(data[3].encode('hex'),8)
-                rospy.logerr("Invalid Encoder Data")
+        # try:
+        # data = self.data
+
+        self.serialIDLE_flag = 0
+        while True:
+            try:
+                data = self.serialQ.get(timeout=10)
+                if(len(data)==10):
+                    if(int(data[3].encode('hex'),16) == 0x08):
+                        break
+                    else:
+                        self.serialQ.put(data)
+                        rospy.logerr("Not Batter Msg")
+                elif (len(data) > 10):
+                    if(int(data[3].encode('hex'),16) == 0x08):
+                        for i in range(0,len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        self.serialQ.put(data[int(data[1].encode('hex'),16):])
+                        for i in range(int(data[1].encode('hex'),16),len(data)):
+                            print "%x"%int(data[i].encode('hex'),16),
+                        print '\n'
+                        rospy.logerr("Battery Pest Other Msg")
+                        break
+                    else:
+                        self.serialQ.put(data)
+                else:
+                    self.serialQ.put(data)
+                    rospy.logerr("Not Battery Msg Len %d"%len(data))
+            except:
+                rospy.logerr("Battery Get Q Faild")
+                break
+        rospy.loginfo("Battery Get Q")
+        #Normal mode
+        if(int(data[3].encode('hex'),16) == 0x08):
+            Vvoltage = int(data[4].encode('hex'),16)*256
+            Vvoltage += int(data[5].encode('hex'),16)
+            Icurrent = int(data[6].encode('hex'),16)*256
+            Icurrent += int(data[7].encode('hex'),16)
+            # rospy.loginfo("Valid Data")
+        else:
+            print len(data),int(data[3].encode('hex'),8)
+            rospy.logerr("Invalid Battery Data")
             # self.current_time = rospy.Time.now()
             # dt = (self.previous_time - self.current_time).to_sec
             # self.previous_time = self.current_time
-        except :
-            rospy.logerr("Encoder Try Faild")
-            # print data + str(len(data))
-            pass
+        # except :
+        #     rospy.logerr("Encoder Try Faild")
+        #     # print data + str(len(data))
+        #     pass
         msg = BatteryState()
         msg.header.stamp = self.current_time
         msg.header.frame_id = self.baseId
@@ -245,18 +313,18 @@ class BaseControl:
             chr(0x00) + chr(0x00) + \
             chr((int(rotat_z*1000.0)>>8)&0xff) + chr(int(rotat_z*1000.0)&0xff) + \
             chr(0x00) + chr(0x00)
-        while self.serialIDLE_flag==False:
+        while self.serialIDLE_flag:
             # time.sleep(1)
-            # rospy.loginfo("CmdCB Serial Busy")
+            # rospy.loginfo("CmdCB Serial Busy %d"%self.serialIDLE_flag)
             time.sleep(0.01)
-        self.serialIDLE_flag = False
+        self.serialIDLE_flag = 4
         try:
             while self.serial.out_waiting:
                 pass
             self.serial.write(output)
         except:
             rospy.logerr("Vel Command Send Faild")
-        self.serialIDLE_flag = True
+        self.serialIDLE_flag = 0
         # rospy.loginfo("Send Vel Cmd")
 
 if __name__=="__main__":
